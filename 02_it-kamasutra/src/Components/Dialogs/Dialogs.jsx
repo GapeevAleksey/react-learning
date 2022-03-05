88f22331bccd89ccfb9c875b5d0903ec1669a49e
Gapeev <dizzikz@gmail.com>
2022-03-05 13:36:50 +0300

    Default commit

---
+++ 02_it-kamasutra/src/Components/Dialogs/Dialogs.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import s from './Dialog.module.css';
+
+const Dialogs = (props) => {
+  return <div className="dialogs">React</div>;
+};
+
+export default Dialogs;
--- 02_it-kamasutra/src/Components/Profile/Profile.jsx
+++ 02_it-kamasutra/src/Components/Profile/Profile.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import s from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts';
 
-const Profile = () => {
+const Profile = (props) => {
   return (
-    <nav className={s.profile}>
+    <div className="profile">
       <div>
         <img src="https://i.pinimg.com/736x/b1/93/8e/b1938e033f60606863defdc3d631d4f8.jpg" />
       </div>
       <div className={s.item}>AVA</div>
       <MyPosts />
-    </nav>
+    </div>
   );
 };
 
